801177018fdb498b39b00cbda281be9ead3db688
torn75 <torn_75@yahoo.com>
2019-03-12 23:28:30 -0400

    added more to database

--- customer.js
+++ customer.js
@@ -1,5 +1,5 @@
 var inquirer = require("inquirer");
-var sql = require("mysql");
+var mysql = require("mysql");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -22,26 +22,68 @@ connection.connect(function(err) {
               type: "list",
               name: "item",
               message: "Hello, would you like to view our inventory?",
-              choices: ["Yes, show me your inventory!", "no,  not interested"]  
+              choices: ["Yes, show me your inventory!", "No,  not interested"]  
           },
       ]).then(function(answer){
           if(answer.item === "Yes, show me your inventory!"){
-              console.log("it works")
-              displayInventory();
+            // console.log("it works")
+            displayInventory();
           }else{
-                console.log("Answer is no")
-              connection.end();
+            console.log("Thank you for stopping by!")
+            connection.end();
           }
       })
   }
 
-  askUser();
+//   askUser();
 
   function displayInventory(){
       connection.query("SELECT * FROM products", function(err, res){
-        console.log("\n\n Items For Sale: \n");
+        console.log("\nItems For Sale: ");
         for (var i = 0; i < res.length; i++) {
-
+            console.log("\n");
+            console.log("ID: " + res[i].id);
+            console.log("Product Name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price " + res[i].price);
+            console.log("Stock Quantity " + res[i].stock_quantity);
         }
+        console.log("\n\n\n")
+        inquirer.prompt([
+            {
+                name: "productID",
+                type: "list",
+                message: "Which product would you like to buy:choose the ID?",
+                choices: [
+                    "1","2","3","4","5","6","7","8","9"
+            ]},
+            {
+                name: "total",
+                type: "input",
+                message: "Enter how many you wish to purchase.",
+                validate: function(value){
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                    return false;
+                }
+            }]).then(function(answer) {
+                connection.query("SELECT product_name,department_name,price,stock_quantity FROM products WHERE ?",{id: answer.productID}, function(err,res) {
+                    if(err) throw err;
+                    if(answer.total > res[0].stock_quantity || res[0].stock_quantity < 0){
+                        console.log("\nWe have run out of your requested product.\n");
+                        return askUser();
+                    }
+                    var subtract = res[0].stock_quantity - answer.total;
+                    var totalBought = answer.total * res[0].price;
+                    updateDatabase(subtract, totalBought, answer)
+                })
+            });
+      })
+  }
+
+  function updateDatabase(subtract, totalBought, answer) {
+      connection.query("UPDATE products SET stock_quantity=? WHERE id=?", [subtract, answer.productID], function (err, res){
+          if(err) throw err;
       })
   }
